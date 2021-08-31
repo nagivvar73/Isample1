92c9da78e82af756ae47000357f1f54ea9bd6012
veeravenkataap <veeravenkataap@outlook.com>
2021-08-31 08:44:32 +0530

    Version1

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,15 @@ class Simple{
     public static void main(String args[]){
         System.out.println("Hello Java1");
         System.out.println("Hello Java2");
+        Addition ad=new Addition();
+        ad.Add();
+    }
+}
+
+class Addition{
+    public void  Add()
+    {
+        System.out.println("I am coming from Add Function");
+
     }
 }
